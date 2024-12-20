26b76445170d21252e14ae6216a30624b452fb06
45705879+ChauhanJaved@users.noreply.github.com <chauhanjaved@gmail.com>
2024-12-20 19:25:14 +0530

    portfolio component update

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -51,7 +51,7 @@ export default function Portfolio(props: { galleryID: string }) {
                 className="flex w-full flex-col items-center justify-center p-4 md:w-1/2 lg:w-1/3"
               >
                 <Link href={item.pageLink}>
-                  <div className="flex w-[300px] flex-col items-center justify-center rounded border py-3">
+                  <div className="flex w-[300px] flex-col items-center justify-center rounded border py-3 hover:shadow-lg active:shadow-lg">
                     <div className="relative mt-3 flex h-[250px] w-[250px] items-center justify-center overflow-hidden rounded bg-primary-foreground">
                       <Image
                         src={item.src}
@@ -61,9 +61,7 @@ export default function Portfolio(props: { galleryID: string }) {
                       />
                     </div>
                     <p className="mt-3 text-xs">{item.categories[1]}</p>
-                    <Button variant={"outline"} className="mt-2">
-                      {item.title}
-                    </Button>
+                    <p className="mt-2 text-sm font-bold">{item.title}</p>
                   </div>
                 </Link>
               </div>
