5dd9d4327a574db59a09c4aa46357b519c9d0b2a
ChauhanJaved <45705879+ChauhanJaved@users.noreply.github.com>
2025-11-07 12:28:05 +0530

    footer update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,6 @@
 // External imports
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 interface PropsFooter {
   companyName: string;
@@ -8,17 +9,39 @@ interface PropsFooter {
 
 export default function Footer(props: PropsFooter) {
   return (
-    <footer className="mt-10 border-t">
-      <div className="mx-5 mb-16 mt-5 flex flex-col items-center gap-1 text-center text-base sm:mx-7 md:mx-9 lg:mx-11">
-        <p>
-          © {props.copyrightYear}
-          {", "}
-          {props.companyName}
-        </p>
-        <div className="flex flex-row gap-1">
-          <Link href="/terms-of-use">Terms of Use</Link>
+    <footer className="mt-10 border-t text-sm">
+      <div className="mx-5 mb-16 mt-5 flex flex-col items-center gap-3 text-center sm:mx-7 md:mx-9 lg:mx-11">
+        <div className="flex flex-row flex-wrap items-center justify-center gap-3">
+          <Button
+            variant={"link"}
+            className="h-auto px-0 py-0"
+            aria-label="Go to company website"
+            asChild
+          >
+            <Link href={"/"}>
+              © {props.copyrightYear} {props.companyName}
+            </Link>
+          </Button>
+        </div>
+
+        <div className="flex flex-row">
+          <Button
+            asChild
+            variant={"link"}
+            className="h-auto px-0 py-0"
+            aria-label="Go to company website for terms of use"
+          >
+            <Link href="/terms-of-use">Terms of Use</Link>
+          </Button>
           <span className="mx-2">|</span>
-          <Link href="/privacy-policy">Privacy Policy</Link>
+          <Button
+            variant={"link"}
+            className="h-auto px-0 py-0"
+            aria-label="Go to company website for privacy policy"
+            asChild
+          >
+            <Link href="/privacy-policy">Privacy Policy</Link>
+          </Button>
         </div>
       </div>
     </footer>
